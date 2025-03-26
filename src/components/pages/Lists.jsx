95228ab7d74d974fa3cf8f0d43de61cba95b9eba
poornima20 <poornima20suresh@gmail>
2025-03-27 01:33:03 +0530

    Initial commit V0.2 - Popup and Responsive Table

--- src/components/pages/Lists.jsx
+++ src/components/pages/Lists.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './Lists.css';
 
 const table_contents = [
-  {bookmark: false, number: '1 .', title: 'AI-Generated Show Recommendations', category: 'Shows'},
-  {bookmark: false, number: '2 .', title: 'Essential DSA Practice Questions', category: 'Study'},
-  {bookmark: false, number: '3 .', title: 'Personal Watchlist Table', category: 'Shows'},
+  {bookmark: false, number: '1 .', title: 'AI-Generated Show Recommendations', category: 'Shows', page: "public/Projects/AI-Show-Recs/index.html"},
+  {bookmark: false, number: '2 .', title: 'Essential DSA Practice Questions', category: 'Study' , page: "public/Projects/AI-Show-Recs/index.html"},
+  {bookmark: false, number: '3 .', title: 'Personal Watchlist Table', category: 'Shows', page: "public/Projects/AI-Show-Recs/index.html"},
 ];
 
 export function Lists(){
+
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [popupPage, setPopupPage] = useState("");
+
+    const openPopup = (pageUrl) => {
+      setPopupPage(pageUrl);
+      setPopupVisible(true);
+    };
+
+    const closePopup = () => {
+      setPopupVisible(false);
+    }
+
+
+
     return (
         <div className="table-container">
           <h2>BuildTab</h2>
@@ -30,11 +45,27 @@ export function Lists(){
                     <td className="number">{content.number}</td>
                     <td className="title">{content.title}</td>
                     <td className="category">{content.category}</td>
-                    <td className="arrow"></td>
+                    <td>
+                      <button className="open-btn arrow" onClick={() => openPopup(content.page)}>-</button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
-          </table>        
+          </table>   
+
+           {/*Popup Modal*/}  
+            {popupVisible && (
+            <div className="popup-overlay" onClick={closePopup}>
+                <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+                    <div className="popup-header">
+                      <button className="open-new-tab-btn" onClick={() => window.open(popupPage, "_blank")}>🔗</button>
+                        <span className="close-btn" onClick={closePopup}>&times;</span>
+                            
+                    </div>
+                    <iframe src={popupPage} frameBorder="0"></iframe>
+                </div>
+            </div>
+            )}
         </div>
       );
 };
