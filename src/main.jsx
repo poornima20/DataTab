1c93a6ea73e9d88b6715e1cadbfb505f5ad6ed8a
poornima20 <poornima20suresh@gmail>
2025-06-25 18:57:00 +0530

    New changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client';
-//import ReactDOM from "react-dom/client";
-import React from "react";
+import React from 'react';
 import './index.css';
 import App from './App.jsx';
-import {BrowserRouter} from "react-router-dom";//for link
+import { BrowserRouter } from 'react-router-dom'; // for routing
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+  <BrowserRouter basename="/DataTab">
     <App />
-</BrowserRouter>
+  </BrowserRouter>
 );
